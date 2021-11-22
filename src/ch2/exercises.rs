24c6ffc55b2c4dce73de6ff56081a6bd3a4bfa29
Pavel Mocan <pavel-ovidiu.mocan@erstegroup.com>
2021-11-22 23:31:56 +0100

    Half of a square and sideways triangle

--- src/ch2/exercises.rs
+++ src/ch2/exercises.rs
@@ -1,7 +1,29 @@
 fn half_of_a_square() {
-    println!("#####");
+    println!("Half of a square:");
+
+    for i in 1..6 {
+        for _hash_num in 1..(6 - i + 1) {
+            print!("#");
+        }
+        println!();
+    }
+}
+
+fn sideways_triangle() {
+    println!("Sideways triangle:");
+
+    for row in 1..8 {
+        let empty_spaces = i16::abs(4 - row);
+        let hashes = 4 - empty_spaces;
+
+        for _hash_num in 1..(hashes + 1) {
+            print!("#");
+        }
+        println!();
+    }
 }
 
 pub fn run() {
     half_of_a_square();
+    sideways_triangle();
 }
